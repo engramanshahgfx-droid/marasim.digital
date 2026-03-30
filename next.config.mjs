5b48a4f98676dc386a1274707027ef1c3980188a
engr-amanshah <engramanshahgfx@gmail.com>
2026-03-30 05:59:41 +0300

    added

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 
 const nextConfig = {
+  output: 'export',
   reactStrictMode: false,
   images: {
+    unoptimized: true,
     minimumCacheTTL: 2678400 * 6, // 3 months
     remotePatterns: [
       {
